dbf954e0b5e42fcebd029f14f60bf458995722d8
thytran38 <ax4409h@gmail.com>
2021-10-14 21:31:43 +0700

    Change directory path

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const app = express();
 
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/'));
+app.use(express.static('./dist/dashboard-admin'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/angular-app-heroku/'}),
+    res.sendFile('index.html', {root: 'dist/dashboard-admin/'}),
 );
 
 // Start the app by listening on the default Heroku port
